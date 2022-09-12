87e830893f471e55cebdfb87f20d0acfee70b860
Aiano_czm <Aiano_czm@163.com>
2022-09-13 00:19:06 +0800

    printf float

--- FOC/FOC.c
+++ FOC/FOC.c
@@ -153,7 +153,8 @@ void FOC_open_loop_voltage_control_loop(float Uq) {
     FOC_SVPWM(Uq, 0, electrical_angle);
 
     // debug
-    printf("%d,%d,%d\n", (int16_t) cs_value[0], (int16_t)cs_value[1], (int16_t)cs_value[2]);
+//    printf("%d,%d,%d\n", (int16_t) cs_value[0], (int16_t)cs_value[1], (int16_t)cs_value[2]);
+    printf("%.1f,%.1f,%.1f\n", cs_value[0], cs_value[1], cs_value[2]);
 }
 
 /**
